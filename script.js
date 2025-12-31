3c12c33b3e0fecad283b4a5ee83fa35dd0d1c2ba
nivas7920 <118712716+nivas7920@users.noreply.github.com>
2025-12-31 12:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ function updateTimer() {
+            const now = new Date();
+            document.getElementById("timer").innerText = now.toLocaleString();
+        }
+
+       
+        updateTimer();
+
+        
+        setInterval(updateTimer, 1000);
